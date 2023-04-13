fff4cdf0edb2efe69ae8baa0ef84eeda3f3e14e5
acuevaEC <andres.cueva.c@gmail.com>
2023-04-13 00:09:18 -0500

    FiltroTabla

--- 1PF_Cueva/src/app/components/tablas/tablas.component.ts
+++ 1PF_Cueva/src/app/components/tablas/tablas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 interface Estudiante {
   id: number;
   nombre: string;
-  apellido: string,
+  apellido: string;
   ci: string;
   email: string;
 }
@@ -11,33 +12,42 @@ interface Estudiante {
 @Component({
   selector: 'app-tablas',
   templateUrl: './tablas.component.html',
-  styleUrls: ['./tablas.component.css']
+  styleUrls: ['./tablas.component.css'],
 })
 export class TablasComponent {
-
-  estudiantes: Estudiante[]=[
+  estudiantes: Estudiante[] = [
     {
       id: 1,
       nombre: 'Andres',
-      apellido:'Cueva',
-      ci:'1722303245',
-      email:'andres.cueva.c@gmail.com'
+      apellido: 'Cueva',
+      ci: '1722303245',
+      email: 'andres.cueva.c@gmail.com',
     },
     {
       id: 2,
       nombre: 'Pablo',
-      apellido:'Cueva',
-      ci:'1714000659',
-      email:'pb.cueva@gmail.com'
+      apellido: 'Cueva',
+      ci: '1714000659',
+      email: 'pb.cueva@gmail.com',
     },
     {
       id: 3,
       nombre: 'Daniela',
-      apellido:'Rios',
-      ci:'1578258629',
-      email:'daniela.r.ec@gmail.com'
+      apellido: 'Rios',
+      ci: '1578258629',
+      email: 'daniela.r.ec@gmail.com',
     },
-  ]
-  displayedColumns: string[]=['id','nombre','apellido','ci','email'];
-}
+  ];
 
+  dataSource = new MatTableDataSource(this.estudiantes);
+
+  displayedColumns: string[] = ['id', 'nombre', 'apellido', 'ci', 'email'];
+
+  aplicarFiltro(ev: Event): void {
+    
+    const inputValue = (ev.target as HTMLInputElement)?.value;
+    this.dataSource.filter = inputValue?.trim()?.toLowerCase();
+    
+  }
+  
+}
--- 1PF_Cueva/src/app/components/tablas/tablas.module.ts
+++ 1PF_Cueva/src/app/components/tablas/tablas.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablasComponent } from './tablas.component';
 import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -12,6 +14,8 @@ import {MatTableModule} from '@angular/material/table';
   imports: [
     CommonModule,
     MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
     
   ],
   exports:[
